81b4d50fef736623cf96e5839b0528c6061d6ae2
adensW <1012790248@qq.com>
2017-11-26 17:21:42 +0800

    create orm.py

--- www/app.py
+++ www/app.py
@@ -1,9 +1,13 @@
+
+'''
+    Python web app 
+'''
+
 #!usr/bin/env python3
 #-*- coding :utf-8 -*-
 
 __author__="adensW"
 import logging;logging.basicConfig(level=logging.INFO)
-
 import asyncio ,os,json,time
 from datetime import datetime
 from aiohttp import web
