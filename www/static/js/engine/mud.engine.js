8618a43af627186ec84d84ca0031fcdaa5f0ae93
adensW <1012790248@qq.com>
2017-12-04 22:50:15 +0800

    add typing text display

--- www/static/js/engine/mud.engine.js
+++ www/static/js/engine/mud.engine.js
@@ -12,13 +12,14 @@
 // engine.start();
 var engine_screen = "<div class=\"screen-bg\"><div class=\"screen-display\"><\/div><div class=\"interactive\"><\/div><\/div>";
 function engine(){
-    this.count = 0;
+    
     this.screen;
     this.interactive;
     this.start = function(){
-        alert('engine start ' + this.count)
+        console.log('engine start ')
     }
     this.init = function(v){ 
+        console.log('engine start ')
         var engine_div =  document.getElementById(v);
         // remove_children(this.engine_div);
         engine_div.innerHTML="";
@@ -26,6 +27,7 @@ function engine(){
         screen_bg.setAttribute('class','screen-bg');
         this.screen = document.createElement("div");
         this.screen.setAttribute('class','screen-display')
+        this.screen.setAttribute('id','screen')
         screen_bg.appendChild(this.screen);
         engine_div.appendChild(screen_bg);
         this.interactive = document.createElement("div")
@@ -38,7 +40,38 @@ function engine(){
     this.getInteractive = function(){
         return this.interactive
     }
+    this.displayText = function(text,place,opt){
+        if(arguments.length==0){
+            var text = document.createElement("p");
+            text.innerHTML = "Waitting..."
+            var p = this.getScreen()
+            p.appendChild(text)
+            
+        }
+        else if(arguments.length==1){
+
+        }else if(arguments.length==2){
+            var count= 0
+            var t
+            var elm_p = document.createElement('p')
+            this.screen.appendChild(elm_p)
+            function typeText(){
+                // alert(elm_p+""+count)
+                elm_p.innerText=text.substr(0,count)
+                // elm_p.append(text.charAt(count))
+                if (count < text.length) {
+                    count++
+                    t = setTimeout(typeText, 100);
+                }
+                else
+                    clearTimeout(t);
+            }
+            typeText();
+        }
+        
+    }
 }
+
 
 function remove_children(parent){
     var _children_count =  parent.childElementCount;
@@ -53,6 +86,6 @@ function remove_children(parent){
 }
 var e = new engine();
 e.init("engine")
-alert(e.getScreen())
+// e.displayText();
 
-
+e.displayText("welcone fellow",1);
